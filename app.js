56748f164439e09116596134709cad0c2c4f6f1b
Abramov Vadim <vadimabramov322@gmail.com>
2024-03-14 22:11:52 +0300

    add cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors')
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors())
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
